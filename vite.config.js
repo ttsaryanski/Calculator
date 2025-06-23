ae7457ffb448276a777cd0ff6ead3ba4ffaf9309
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2025-06-23 19:16:57 +0300

    Deploy fo Firebase and add PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: "autoUpdate",
+            manifest: {
+                name: "Calculator App",
+                short_name: "Calculator",
+                start_url: "/",
+                display: "standalone",
+                background_color: "#ffffff",
+                theme_color: "#2c3e50",
+            },
+        }),
+    ],
+    base: "/",
+    server: {
+        historyApiFallback: true,
+    },
+});
